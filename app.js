b9a49de50fa887162c617c724e54233f95522e20
aaqeebh1 <24283167+aaqeebh1@users.noreply.github.com>
2024-07-06 18:53:19 +0100

    improved name data collection

--- app.js
+++ app.js
@@ -6,30 +6,37 @@ const url = "https://www.ufc.com/events"
 
 const eventFights = []
 
+function cleanText(text) {
+  return text.replace(/\s+/g, ' ').trim();
+}
+
 
 async function getEvents() { 
     try { 
         const response = await axios.get(url);
         const $ = cheerio.load(response.data);
-        const headline = $(".c-hero--full__headline").text();
-        const fightCard = $(".c-hero__actions").find("a").attr("href");
+        const headline = cleanText($(".c-hero--full__headline").text());
+        const fightCard = cleanText($(".c-hero__actions").find("a").attr("href"));
         
         const fightCardResponse = await axios.get(fightCard);
         const $$ = cheerio.load(fightCardResponse.data);
-        const cardStartTime = $$(".c-event-fight-card-broadcaster__time").text();
-        const cardLocation = $$(".c-hero__text").find(".field").text();
+        const cardStartTime = cleanText($$(".c-event-fight-card-broadcaster__time").text())
+        const cardLocation = cleanText($$(".c-hero__text").find(".field").text());
         const fights = $$(".c-listing-fight__content-row");
 
         fights.each(function () {
-            const fighterNameHrefRedCorner = $(this).find(".c-listing-fight__corner-name--red").find("a").text();
-            const fightRedCorner = $(this).find(".c-listing-fight__corner-name--red")
-            const fighterNameRedCorner = fightRedCorner.find(".c-listing-fight__corner-given-name").text() + "" + fightRedCorner.find(".c-listing-fight__corner-family-name").text();
-            const fighterNameHrefBlueCorner = $(this).find(".c-listing-fight__corner-name--blue").find("a").text();
+            const fightRedCorner = $(this).find(".c-listing-fight__corner-name--red")            
+            const fighterNameRedCorner = cleanText($(this).find(".c-listing-fight__corner-name--red").find("a").text()) || fightRedCorner.find(".c-listing-fight__corner-given-name").text() + " " + fightRedCorner.find(".c-listing-fight__corner-family-name").text();
             const fightBlueCorner = $(this).find(".c-listing-fight__corner-name--blue")
-            const fighterNameBlueCorner = fightBlueCorner.find(".c-listing-fight__corner-given-name").text() + " " + fightBlueCorner.find(".c-listing-fight__corner-family-name").text();
-            console.log(`${!fighterNameRedCorner ? fighterNameHrefRedCorner : fighterNameRedCorner} vs ${!fighterNameBlueCorner ? fighterNameHrefBlueCorner : fighterNameBlueCorner}`)
+            const fighterNameBlueCorner = cleanText($(this).find(".c-listing-fight__corner-name--blue").find("a").text()) || fightBlueCorner.find(".c-listing-fight__corner-given-name").text() + " " + fightBlueCorner.find(".c-listing-fight__corner-family-name").text();
+            eventFights.push(`${fighterNameRedCorner} vs ${fighterNameBlueCorner}`)
         });
         
+        
+        console.log(`${headline}
+             ${cardLocation}
+              ${cardStartTime}
+               `, eventFights)
 
         
         
